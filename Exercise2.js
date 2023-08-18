e7a27fc32fcb583c21edfc1eee7fa14eb3d98351
sgtclark90 <142550430+sgtclark90@users.noreply.github.com>
2023-08-17 20:49:46 -0500

    Exercise2

---
+++ Exercise2.js
@@ -0,0 +1,7 @@
+//Exercise 2
+//console.log('Hello World!')
+/*console.log("There once was a man on a computer")
+console.log('As he did not know what to do')
+console.log("Code they said, It will be fun")
+console.log('Not knowing which is foot or shoe')*/
+
