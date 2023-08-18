dfb3f85bf15dd1c9b3ddde15591c4f9d7272429b
sgtclark90 <142550430+sgtclark90@users.noreply.github.com>
2023-08-17 22:13:15 -0500

    Exercise 5

---
+++ Exercise5.js
@@ -0,0 +1,11 @@
+console.log("True or false: 5 is less than 2");
+console.log(5 < 2);
+console.log("True or false: 19 is not equal 11");
+console.log(19 != 11)
+console.log("True or false: 10 is greater than or equal to 10");
+console.log(10 >= 10);
+
+console.log(10 == 10); // ==, true
+console.log(10 > 15); // >, false
+console.log(10 <= 7); // <=, false
+console.log(10 != 15); // !=, true
