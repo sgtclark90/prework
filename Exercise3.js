43a1ed624603a4ee8bfd8f61d204754c445570c1
sgtclark90 <142550430+sgtclark90@users.noreply.github.com>
2023-08-17 21:47:59 -0500

    Exercise3.jv

---
+++ Exercise3.js
@@ -0,0 +1,12 @@
+//Exercise 3
+console.log("This is a string, it has quotes.");
+console.log(100);
+console.log("100");
+console.log("My name is: ");
+console.log("Michael Clark"); //change this line
+console.log("My age is: ");
+console.log(32); //change this line, which type should you use?
+console.log("My zip code is: "); 
+console.log(71841); //change this line, which type should you use?
+
+
