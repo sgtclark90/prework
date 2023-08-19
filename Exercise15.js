f23946485ebecae564f02bb6ee0f268d780e177f
sgtclark90 <142550430+sgtclark90@users.noreply.github.com>
2023-08-18 21:09:15 -0500

    Exercise 15

---
+++ Exercise15.js
@@ -0,0 +1,58 @@
+// console.log(`Hello, Guest Number ${1}`);
+// console.log(`Hello, Guest Number ${2}`);
+// console.log(`Hello, Guest Number ${3}`);
+// console.log(`Hello, Guest Number ${4}`);
+// console.log(`Hello, Guest Number ${5}`);
+// console.log(`Hello, Guest Number ${6}`);
+// console.log(`Hello, Guest Number ${7}`);
+// console.log(`Hello, Guest Number ${8}`);
+// console.log(`Hello, Guest Number ${9}`);
+// console.log(`Hello, Guest Number ${10}`);
+
+
+
+// const drinksMenu = ["soda", "tea", "coffee", "juice", "milk", "water"]
+
+// console.log("Welcome in! Here are your drink options:");
+
+// console.log(drinksMenu[0]);
+// console.log(drinksMenu[1]);
+// console.log(drinksMenu[2]);
+// console.log(drinksMenu[3]);
+// console.log(drinksMenu[4]);
+// console.log(drinksMenu[5]);
+
+
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`Now we're on number ${i}`);
+// }
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log("This is going to be logged 10 times.");
+//     console.log("And it's the same every time.");   
+// }
+
+
+
+// const drinksMenu = ["soda", "tea", "coffee", "juice", "milk", "water"]
+
+// console.log("Welcome in! Here are your drink options:");
+// for (let i in drinksMenu) {
+//     console.log(drinksMenu[i]);
+// }
+
+
+
+//Write a loop that displays the square and the cube of each integer from 0 up to 5, inclusive
+//Remember the exponentiation operator **
+
+for (i = '0'; i <= '5'; i++) {
+    let square = i ** 2;
+    let cube = i ** 3;
+
+    console.log(`The square of ${i} is ${square}`);
+    console.log(`The cube of ${i} is ${cube}`)
+}
